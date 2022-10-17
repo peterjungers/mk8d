1e7dfd075b2575cb20a13046dce8db8fc78f230a
Peter Jungers <peter.jungers@pcc.edu>
2022-10-16 18:26:32 -0700

    Initial commit

---
+++ run.py
@@ -0,0 +1,20 @@
+"""
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Author:           Peter Jungers
+Program name:     Mario Kart 8 Deluxe Cup Generator
+Version:	        1.0
+Date:             October 2022
+Description:      Website that allows user to view cups I have created in
+                  Mario Kart 8 Deluxe, which are based on various themes
+                  found across tracks in the game.
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+"""
+
+
+from mk8d import create_app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
