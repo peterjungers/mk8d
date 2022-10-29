bc57cfe863a4ebaecb614a366ef2e4ad2b2cc1ea
Peter Jungers <peter.jungers@pcc.edu>
2022-10-29 13:09:31 -0700

    Finished version 1.0

--- mk8d/__init__.py
+++ mk8d/__init__.py
@@ -4,11 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config['SQLALCHEMY_ECHO'] = False
 db = SQLAlchemy(app)
 
 from mk8d import handlers
 from mk8d import models
 from mk8d import routes
-
-
--- run.py
+++ run.py
@@ -13,4 +13,4 @@ from mk8d import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
